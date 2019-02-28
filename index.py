b40c7f0add31694fcdc89aa3e2a1f026ff5c1faf
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2019-02-28 23:03:34 +0530

    Added very basic stuff.

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
 import cherrypy
-# from pymongo import MongoClient
+from json import load
+from os import path
+from pymongo import MongoClient
+
+# Read configuration.
+mongo_url = 'mongodb://localhost:27017'
+if path.exists('config.json'):
+    mongo_url = load(open('config.json'))['MONGO_URL']
+
+# Connect to MongoDB.
+client = MongoClient(mongo_url)
+cherrypy.log('Connected to MongoDB!')
 
 
 class Root:
     @cherrypy.expose
     def index(self):
-        return 'Hello, world! (lmao)'
+        return '<head><title>test</title></head><body><p>Hello, world!</p><p>\
+            WIP</p></body>'
+
+    @cherrypy.expose
+    def messages(self):
+        # Just testing stuff..
+        return str(client.ezchat.messages.find({}).count)
 
 
 if __name__ == '__main__':
