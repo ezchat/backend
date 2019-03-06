dce695dd85779bac10c652a6141549d7666d2e1d
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2019-03-06 21:39:56 +0530

    Better Snowflakes.

--- api/snowflake.py
+++ api/snowflake.py
@@ -7,13 +7,13 @@ class Snowflake:
         self.increment = 0
 
     def generate_snowflake(self):
-        time = time_ns()
+        time = time_ns() / 1000
         if self.last_epoch == time:
             self.increment += 1
         else:
             self.last_epoch = time
             self.increment = 0
-        # Generate a Snowflake computing 61 bit epoch and 7 bits of increment.
+        # Generate a Snowflake computing 51 bit epoch and 7 bits of increment.
         binary_epoch = bin(time)
         binary_increment = bin(self.increment)[2:]
         extra_bits = ((7 - len(binary_increment)) * '0')
